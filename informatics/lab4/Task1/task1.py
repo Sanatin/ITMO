03c557895444d6934a6ba75e3a5adc340d042524
Dmitriy612 <50769890+Sanatin@users.noreply.github.com>
2024-11-21 20:53:16 +0300

    Update task1.py

--- informatics/lab4/Task1/task1.py
+++ informatics/lab4/Task1/task1.py
@@ -1,17 +1,34 @@
-import xmltodict
-import yaml
+import time
 
+import Task0.task0 as task0
+import Task1.task1 as task1
+import Task2.task2 as task2
+import Task3.task3 as task3
 
-def xml_to_yaml(xml_file_path, yaml_file_path):
+print('Время основного задания:')
+start = time.time()
+for i in range(100):
+   task0.xml_to_yaml('../schedule.xml', '../Task0/schedule.yaml')
+end = time.time() - start
+print(end)
 
-    xml_file = open(xml_file_path, "r", encoding="utf-8")
-    yaml_file = open(yaml_file_path, "w", encoding="utf-8")
+print('Время доп задания 1:')
+start = time.time()
+for i in range(100):
+   task1.xml_to_yaml('../schedule.xml', '../Task1/schedule.yaml')
+end = time.time() - start
+print(end)
 
-    doc = xmltodict.parse(xml_file.read())
+print('Время доп задания 2:')
+start = time.time()
+for i in range(100):
+   task2.xml_to_yaml('../schedule.xml', '../Task2/schedule.yaml')
+end = time.time() - start
+print(end)
 
-    yaml_file.write(yaml.dump(doc, allow_unicode=True))
-    yaml_file.close()
-
-
-if __name__ == '__main__':
-    xml_to_yaml('../schedule.xml', 'schedule.yaml')
+print('Время доп задания 3:')
+start = time.time()
+for i in range(100):
+   task3.xml_to_yaml('../schedule.xml', '../Task3/schedule.yaml')
+end = time.time() - start
+print(end)
