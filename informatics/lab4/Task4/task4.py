8f53b3190662734ee82bf9917934d5e60db3db27
Dmitriy612 <50769890+Sanatin@users.noreply.github.com>
2024-11-21 21:24:12 +0300

    Update task4.py

--- informatics/lab4/Task4/task4.py
+++ informatics/lab4/Task4/task4.py
@@ -1,33 +1,31 @@
 import time
+
 import Task0.task0 as task0
 import Task1.task1 as task1
 import Task2.task2 as task2
 import Task3.task3 as task3
 
-print('Время основного задания:')
 start = time.time()
 for i in range(100):
-   task0.xml_to_yaml('../schedule.xml', 'schedule.yaml')
+   task0.xml_to_yaml('../schedule.xml', '../Task0/schedule.yaml')
 end = time.time() - start
-print(end)
+print(f"Время основного задания: {end}")
 
-print('Время доп задания 1:')
 start = time.time()
 for i in range(100):
-   task1.xml_to_yaml('../schedule.xml', 'schedule.yaml')
+   task1.xml_to_yaml('../schedule.xml', '../Task1/schedule.yaml')
 end = time.time() - start
-print(end)
+print(f"Время доп задания 1: {end}")
 
-print('Время доп задания 2:')
 start = time.time()
 for i in range(100):
-   task2.xml_to_yaml('../schedule.xml', 'schedule.yaml')
+   task2.xml_to_yaml('../schedule.xml', '../Task2/schedule.yaml')
 end = time.time() - start
-print(end)
+print(f"Время доп задания 2: {end}")
 
-print('Время доп задания 3:')
+
 start = time.time()
 for i in range(100):
-   task3.xml_to_yaml('../schedule.xml', 'schedule.yaml')
+   task3.xml_to_yaml('../schedule.xml', '../Task3/schedule.yaml')
 end = time.time() - start
-print(end)
+print(f"Время доп задания 3: {end}")
